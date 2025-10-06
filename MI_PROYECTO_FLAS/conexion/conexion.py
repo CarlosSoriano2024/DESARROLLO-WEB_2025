2ffa400c6394940633eb5800af15b43d5ca00cb6
CarlosSoriano2024 <159310252+CarlosSoriano2024@users.noreply.github.com>
2025-10-05 22:39:56 -0500

    conexion.py

--- MI_PROYECTO_FLAS/conexion/conexion.py
+++ MI_PROYECTO_FLAS/conexion/conexion.py
@@ -1,1 +1,10 @@
+import mysql.connector
+
+def conectar():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="libreria_don_danny"
+    )
 
