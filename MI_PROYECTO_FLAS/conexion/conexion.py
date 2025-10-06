a842ced58b1557643f31cf554229a8526c3ecd99
CarlosSoriano2024 <159310252+CarlosSoriano2024@users.noreply.github.com>
2025-10-05 21:40:57 -0500

    conexion.py

---
+++ MI_PROYECTO_FLAS/conexion/conexion.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+def conectar():
+    try:
+        conexion = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",  # vacío por defecto en XAMPP
+            database="libreria_don_danny"
+        )
+        return conexion
+    except mysql.connector.Error as error:
+        print("Error de conexión:", error)
+        return None
